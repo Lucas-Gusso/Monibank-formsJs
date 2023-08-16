a7f25eb1339fd35b6ef5712014a1d67301d7c64c
Lucas <lucasgusso28@gmail.com>
2023-08-16 19:52:41 -0300

    Implementação da lógica para captura de imagens

--- Js/camera.js
+++ Js/camera.js
@@ -2,6 +2,14 @@ const btnIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 
+const btnTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+
+const btnEnviarFoto = document.querySelector("[data-enviar]");
+
+let imagemURL ="";
+
 btnIniciarCamera.addEventListener("click", async function(){
     const iniciarVideo = await navigator.mediaDevices
     .getUserMedia({video: true, audio: false});
@@ -10,4 +18,13 @@ btnIniciarCamera.addEventListener("click", async function(){
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+})
+
+btnTirarFoto.addEventListener("click", function( ){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
 })
